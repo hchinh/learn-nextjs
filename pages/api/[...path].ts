647840f7eb1261bc138dff4b55f56d4854bdc412
Chinh Huynh <chinh.huynh@team.enouvo.com>
2022-03-27 15:55:51 +0700

    forward all reqs to api server

---
+++ pages/api/[...path].ts
@@ -0,0 +1,22 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import httpProxy from 'http-proxy'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+export const config = {
+	api: {
+		bodyParser: false,
+	},
+}
+
+const proxy = httpProxy.createProxyServer()
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<any>) {
+	// don't send cookies to API server
+	req.headers.cookie = ''
+
+	proxy.web(req, res, {
+		target: process.env.API_URL,
+		changeOrigin: true,
+		selfHandleResponse: false,
+	})
+}
